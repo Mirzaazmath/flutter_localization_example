14d3e98e6e147dade6616b281ace7401a639898d
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-08 15:55:40 +0530

    configuration : added localizationsDelegates in our app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_locallization_example/home_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 void main (){
   runApp(MyApp());
 }
@@ -9,6 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+
       home: HomeScreen(),
     );
   }
