760ace39797e2272259c95605121066753b58555
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-08 17:36:18 +0530

    added popup menu button in appbar of home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-class HomeScreen extends StatelessWidget {
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+enum Language { english, spanish }
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.helloWorld),
+        actions: [
+          PopupMenuButton(
+            onSelected: (lang) {},
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(value: Language.english, child: Text("English")),
+                PopupMenuItem(value: Language.spanish, child: Text("Spanish")),
+              ];
+            },
+          ),
+        ],
       ),
     );
   }
