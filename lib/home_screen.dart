6fa9fbce2daca9fcb972f91a6158c4e066b54e28
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-08 16:01:51 +0530

    accessing the localization code in our app in home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.helloWorld),
+      ),
+    );
   }
 }
