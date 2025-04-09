046d39aa0335beaa2f09b8fd59dfc9163867c843
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-09 12:09:30 +0530

    added consumer in popupmenu

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_locallization_example/providers/change_language_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,15 +19,23 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.helloWorld),
         actions: [
-          PopupMenuButton(
-            onSelected: (lang) {},
-            itemBuilder: (BuildContext context) {
-              return [
-                PopupMenuItem(value: Language.english, child: Text("English")),
-                PopupMenuItem(value: Language.spanish, child: Text("Spanish")),
-              ];
-            },
-          ),
+          Consumer<ChangeLanguageProvider>(builder: (context,provider,child){
+            return PopupMenuButton(
+              onSelected: (lang) {
+                if(lang == Language.english){
+                  provider.updateLanguage(Locale('en'));
+                }else{
+                  provider.updateLanguage(Locale('es'));
+                }
+              },
+              itemBuilder: (BuildContext context) {
+                return [
+                  PopupMenuItem(value: Language.english, child: Text("English")),
+                  PopupMenuItem(value: Language.spanish, child: Text("Spanish")),
+                ];
+              },
+            );
+          })
         ],
       ),
     );
