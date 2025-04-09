a7683aedbd74eef732b6953b085b8c85e91d7a89
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-09 12:17:38 +0530

    added SharedPreferences in void main to get the store langCode from Device storage

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,19 @@ import 'package:flutter_locallization_example/screens/home_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences sp = await SharedPreferences.getInstance();
+  String langCode = sp.getString("language_code") ?? "";
+
+  runApp(MyApp(langCode: langCode));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  String langCode;
+  MyApp({super.key, required this.langCode});
 
   @override
   Widget build(BuildContext context) {
@@ -19,20 +25,20 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => ChangeLanguageProvider()),
       ],
       child: Consumer<ChangeLanguageProvider>(
-        builder: (context,provider,child){
+        builder: (context, provider, child) {
           return MaterialApp(
-          locale: provider.appLocale,
-          localizationsDelegates: [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-          ],
-          supportedLocales: [Locale('en'), Locale('es')],
+            locale: provider.appLocale,
+            localizationsDelegates: [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+            ],
+            supportedLocales: [Locale('en'), Locale('es')],
 
-          home: HomeScreen(),
-        );
-        }
+            home: HomeScreen(),
+          );
+        },
       ),
     );
   }
